446f0f3d87a6dd5d9d3d51df2e499450768d56ff
Rigel Ramadhan <rigelramadhan@gmail.com>
2022-10-16 00:30:44 +0700

    Starting to implement cooking guide

---
+++ app/src/main/java/com/bangkit/bahanbaku/presentation/cookingguide/CookingGuideActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.bahanbaku.presentation.cookingguide
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.bahanbaku.R
+
+class CookingGuideActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cooking_guide)
+    }
+}
