a730159334dd58503eb826db1255fe404f302fe7
almedifa <srizalzal@gmail.com>
2022-11-12 17:21:08 +0700

    privacy policy

---
+++ app/src/main/java/com/bangkit/bahanbaku/presentation/privacypolicy/privacy_policy.kt
@@ -0,0 +1,11 @@
+package com.bangkit.bahanbaku.presentation.privacypolicy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class privacy_policy : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_privacy_policy)
+    }
+}
