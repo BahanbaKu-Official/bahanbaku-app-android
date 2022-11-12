e9070103e112fa4e3bd98847e2a5c9a623f1ab2a
almedifa <srizalzal@gmail.com>
2022-11-12 13:50:55 +0700

    Bantuan

---
+++ app/src/main/java/com/bangkit/bahanbaku/presentation/helpdesk/HelpdeskActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit.bahanbaku.presentation.helpdesk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit.bahanbaku.R
+
+class HelpdeskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_helpdesk)
+    }
+}
---
+++ app/src/main/java/com/bangkit/bahanbaku/presentation/helpdesk/HelpdeskViewModel.kt
@@ -0,0 +1,4 @@
+package com.bangkit.bahanbaku.presentation.helpdesk
+
+class HelpdeskViewModel {
+}
